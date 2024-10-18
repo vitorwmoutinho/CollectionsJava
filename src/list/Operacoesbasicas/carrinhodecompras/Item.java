aa627eaa7085d1c22d3b26b4139a051a04889558
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-18 16:46:33 -0300

    Atualização da classe Item, adicionado os metodos (contrutor, get e toString).

--- src/list/Operacoesbasicas/carrinhodecompras/Item.java
+++ src/list/Operacoesbasicas/carrinhodecompras/Item.java
@@ -1,4 +1,17 @@
 package Operacoesbasicas.carrinhodecompras;
 
 public class Item {
+    //attrib
+    private String descricao;
+
+    public Item(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getdescricao() {
+        return descricao;
+    }
+    public String toString(){
+        return descricao;
+    }
 }
