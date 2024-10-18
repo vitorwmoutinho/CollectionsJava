122d8a9dc8f183cb14546a7c82091c43cfbe4c74
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-18 16:52:14 -0300

    Atualizando private list

--- src/list/Operacoesbasicas/carrinhodecompras/CarrinhoDeCompras.java
+++ src/list/Operacoesbasicas/carrinhodecompras/CarrinhoDeCompras.java
@@ -1,4 +1,18 @@
-package list.Operacoesbasicas.carrinhodecompras;
+package Operacoesbasicas.carrinhodecompras;
+
+import Operacoesbasicas.carrinhodecompras.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarrinhoDeCompras {
+    //atrib
+    private List<Item> listaCarrinho;
+
+    public CarrinhoDeCompras(){
+        this.listaCarrinho = new ArrayList<>();
+    }
+
+
+
 }
