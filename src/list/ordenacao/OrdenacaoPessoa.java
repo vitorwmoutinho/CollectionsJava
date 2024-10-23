dab7d1458657e61843b5a448aae55fd6e19d7755
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-23 16:39:20 -0300

    Atualização de ordenação por pesssoa

--- src/list/ordenacao/OrdenacaoPessoa.java
+++ src/list/ordenacao/OrdenacaoPessoa.java
@@ -1,4 +1,32 @@
 package ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoa {
+    //attrib
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa(List<Pessoa> pessoaList) {
+        this.pessoaList = new ArrayList<>();
+
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+      pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaList;
+    }
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
 }
+
--- src/list/ordenacao/Pessoa.java
+++ src/list/ordenacao/Pessoa.java
@@ -1,4 +1,50 @@
 package ordenacao;
 
-public class Pessoa {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Pessoa implements Comparable<Pessoa>{
+    //attrib
+    private String nome;
+    private int idade;
+    private double altura;
+    //Construtor
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+    //getter
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
 }
+class ComparatorPorAltura implements Comparator<Pessoa>{
+    public int compare(Pessoa p1, Pessoa p2){
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+}
+
