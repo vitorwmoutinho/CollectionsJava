a0dc10fe8141cec86746bb0877bb341258c36475
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-23 16:08:22 -0300

    Verificando se o código funciona e se os parametros estão corretos

--- src/list/pesquisa/CatalogoLivros.java
+++ src/list/pesquisa/CatalogoLivros.java
@@ -52,4 +52,43 @@ public class CatalogoLivros {
         return livroPorTitulos;
     }
 
+
+
+    public static void main(String[] args){
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livre 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/list/pesquisa/Livro.java
+++ src/list/pesquisa/Livro.java
@@ -32,4 +32,5 @@ public class Livro {
                 ", anoPublicacao=" + anoPublicacao +
                 '}';
     }
+
 }
