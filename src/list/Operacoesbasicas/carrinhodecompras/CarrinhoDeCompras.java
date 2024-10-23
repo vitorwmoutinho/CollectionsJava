b3246505e0c3063cb53c27a5a4ec3705314bf9cd
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-23 11:41:31 -0300

    Atualização do código: adicionado os itens removerItem, calcularValorTotal e exibirItens

--- src/list/Operacoesbasicas/carrinhodecompras/CarrinhoDeCompras.java
+++ src/list/Operacoesbasicas/carrinhodecompras/CarrinhoDeCompras.java
@@ -1,19 +1,4 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+package Operacoesbasicas.carrinhodecompras;
 
 public class CarrinhoDeCompras {
-    private List<Item> itens;
-
-
-    public CarrinhoDeCompras() {
-        this.itens = new ArrayList<>();
-    }
-    public void adicionarItem(String nome, double preco, int quantidade){
-        Item item = new Item(nome, preco, quantidade);
-        itens.add(item);
-        System.out.println("item adicionado: "+ nome);
-
-    }
-
 }
