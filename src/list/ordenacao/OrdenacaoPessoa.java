1d479b2105fd252c005466f44ec3a290b9b0922d
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-24 10:45:11 -0300

    Utilizando com uma mudança de altura

--- src/list/ordenacao/OrdenacaoPessoa.java
+++ src/list/ordenacao/OrdenacaoPessoa.java
@@ -37,8 +37,8 @@ public class OrdenacaoPessoa {
     public static void main(String[] args) {
         OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
         ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
-        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
-        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.82);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.71);
         ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
 
         System.out.println("Ordenação por idade:");
