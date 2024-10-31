d44c1c75755779ac7b4a13c1e983a1dbe781f626
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 11:21:31 -0300

    Criação da classe AgendaEventos e Evento

--- src/map/src/ordenacaonoMap/AgendaEventos.java
+++ src/map/src/ordenacaonoMap/AgendaEventos.java
@@ -1,4 +1,30 @@
 package ordenacaonoMap;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class AgendaEventos {
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos() {
+        this.eventoMap = new HashMap<>();
+    }
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        eventoMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventoMap);
+    }
+    public void obterProximoEvento(){
+        eventoMap.get()
+
+    }
+
+
+
+
 }
--- src/map/src/ordenacaonoMap/Evento.java
+++ src/map/src/ordenacaonoMap/Evento.java
@@ -1,4 +1,27 @@
 package ordenacaonoMap;
 
 public class Evento {
+    private String nome;
+    private String atracao;
+
+    public Evento(String nome, String atracao) {
+        this.nome = nome;
+        this.atracao = atracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAtracao() {
+        return atracao;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "nome='" + nome + '\'' +
+                ", atracao='" + atracao + '\'' +
+                '}';
+    }
 }
